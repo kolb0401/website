21668438ba19c0fb636a4a321a45eea71076c95c
Jonathan <kolb0401@gmail.com>
2015-05-16 18:21:18 -0400

    typo

--- node/assets/js/modules/page/frag/bio.jsx
+++ node/assets/js/modules/page/frag/bio.jsx
@@ -9,9 +9,9 @@ var frag = React.createClass({
         <div className="container">
             <h2>About Me</h2>
             <p>
-              I am a full stack software engineer for Smarter Travel LLC a subsidiarity of
+              I am a full stack software engineer for Smarter Travel, a subsidiary of
               TripAdvisor. In my free time I am the volunteer IT Manager
-              of <a href="http://ruffstartdogs.org">Ruff Start Rescue</a>, a dog
+              of <a href="http://ruffstartdogs.org">Ruff Start Rescue</a>, a 501c3 dog
               rescue organization. I love to experiment with new technologies,
               both frontend and backend, on my person site. When I am not nerding out
               I enjoy watching hockey, and driving fast cars.
